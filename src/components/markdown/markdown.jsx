b7fa3c5eb603d0ede92c5105b55d3d07c4101cb2
Artem <tyomart@gmail.com>
2023-04-04 23:54:49 +0400

    jquery for html output added

--- src/components/markdown/markdown.jsx
+++ src/components/markdown/markdown.jsx
@@ -1,6 +1,7 @@
 
 import './markdown-styles.less'
 import { useState, useContext, useEffect, useReducer } from 'react'
+import $ from 'jquery';
 
 const log = console.log
 const Markdown = () => {
@@ -17,26 +18,28 @@ const [edit, setEdit] = useState('')
 const [readyTXT, setReadyTXT] = useState('') //finally it is array (JSON)
 const [procTXT, setProcTXT] = useState({tag:'',cont:''})
 
+useEffect(()=>{   
+    let aaa = '<h3>aaa</h3>'
+    const $outP = $('#output') 
+    const html = $outP.html()
+    const newHtml = html + readyTXT
+    $outP.html(newHtml)
+   return 
+},[readyTXT])
+
 const transForm = (txIn) => {
     log('in', txIn)
 if (txIn.substr(0,1) === '#') {   //GET TO REDUCER
     return txIn.replace(/#/m, "<h1>").replace(/\n/m,'</h1>');}
 else return txIn
-    // cut tag to tag-field, cut content to content-field, send both to procTxt
-  
-      
-    //return txIn //addLinebreaks(txIn)
+    
 }
 
-   const handleIn =(e) => {
+const handleIn =(e) => {
 
     setEdit(e.target.value)
-    //const inHandled = edit
-    setReadyTXT(transForm(edit))
+      setReadyTXT(transForm(edit))
 
-    // const handleOut = inHandled.split('\n')
-    // log('inH', handleOut)
-     
      log('t-- ', readyTXT)
         return //setProcTXT(handleOut)
     }
@@ -46,9 +49,9 @@ const Preview = (props) => {
    
     // PREVIEW RETURN
     return <>
-        {eDisp}
-        {/* {eDisp.map((val)=><div>{val.typeTag} - {val.contTag}</div>)} */}
-    {<div>{procTXT.tag}{procTXT.cont}</div>}
+        
+       <div id='output'><p>{}</p></div>
+
     </>
 }
 
@@ -79,14 +82,5 @@ const Preview = (props) => {
 
 export default Markdown;
 
-
-  // const addLinebreaks = (anyString) => {
-    //     return anyString.replaceAll("\n", "<br />!!!\r\n");
-    //   };
-
-
-//array of chars
-//if char from tag-table -> insert tag on Display array
-
-//join Display array
+ 
 
--- src/components/routes/navigator.jsx
+++ src/components/routes/navigator.jsx
@@ -1,22 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { Outlet, Link } from 'react-router-dom';
 
+import'./navigator-style.less'
+
 const Navigator = () => {
     return ( <>
+
+    <div className="nav0-box">
             <Link className='main-link' to='/'> MAIN</Link>
-        <div className="navi-container">
+        
 
 
             <div className="navi-links">
                 <Link className='n-link' to='/calculator'> CALCULATOR</Link>
-            _--_
+           
                 <Link className='n-link' to='/quotamat'> QUOTAMAT</Link>
-                _--_
+                
                 <Link className='n-link' to='/markdown'> MARKDOWN</Link>
             </div>
 
-        </div>
-
+        
+    </div>
 
          <Outlet />
          </>
