70e49aae7dac60911f99dce8f98390aaab699b88
Artem <tyomart@gmail.com>
2023-05-23 15:15:53 +0400

    input fxed, h1h2h3_b avail.

--- src/components/markdown/markdown.jsx
+++ src/components/markdown/markdown.jsx
@@ -1,21 +1,23 @@
-
 import './markdown-styles.less'
-import { useState, useContext, useEffect , useLayoutEffect} from 'react'
+import { useState, useContext, useEffect , useLayoutEffect, useCallback} from 'react'
 import $ from 'jquery';
 
 const log = console.log
 const Markdown = () => {
 
 
-const [edit, setEdit] = useState('') // CHECK edit are added by 1 char, if more USE insertion processing, and for initial text, as well 
+const [preIn, setPreIn] = useState('') // inputing state
+const [edit, setEdit] = useState('') // //Shows Input
  
-//const [edCache, setEdCache] = useState('')
 
-const [readyTXT, setReadyTXT] = useState('') //finally it is array (JSON)
+
+const [readyTXT, setReadyTXT] = useState('') //Shows Output
+const [cache, setCache] = useState([])
+
 
 
 const flagsObj  = 
-    { 
+    {   outputFlag: false,
         h1: false,
         h2: false,
         h3: false,
@@ -28,6 +30,177 @@ const flagsObj  =
     } 
 const [flags, setFlags] = useState( flagsObj )
 
+// ---------------------- engine block----
+
+const lastChar = (string0) => {
+    if (string0.length>0) {
+        return string0.slice(-1)
+    }
+    else return '©'
+}
+const prevChar = (string0) => {
+    if (string0.length>1) {
+        return string0.slice(-2,-1)
+    }
+    else return '©' //TO FIX
+}
+
+ const special = (testedChar) => {
+        
+        const specialSet = ['*','\\','\`','#','i','[',']', '|']
+       return  Boolean(specialSet.reduce((acc,elem) => { return acc=acc+(elem===testedChar)} ,false))
+    }
+
+const convert = (proTag) => {
+
+    const tagger = (tag) => { // open/closed tag substitution
+        
+        //exclusion cases are expected here
+
+        log('outval fired')
+       return !flags[`${tag}`]         //basic case
+            ? `<${tag}>` : `</${tag}>`   
+    }
+   
+    // tag cases
+    if (proTag === '#')                     // <h1> tag
+            {log('# fired')
+                setFlags({...flags, h1:!(flags.h1)})
+            return tagger('h1',0);
+            }
+    if (proTag === '##')                     // <h1> tag
+            {log('# fired')
+                setFlags({...flags, h2:!(flags.h2)})
+            return tagger('h2',0);
+            }
+    if (proTag === '###')                     // <h1> tag
+            {log('# fired')
+                setFlags({...flags, h3:!(flags.h3)})
+            return tagger('h3',0);
+            }
+    if (proTag === '*')                     // <h1> tag
+            {setFlags({...flags, b:!(flags.b)})
+            return tagger('b',0);
+            }
+  
+    else return proTag
+} 
+
+const processorTXT = (inVal, out) => { // MOST HOT ISSUE - from where I have to take value to OUTPUT to  show processed only text 
+
+    log('lastChar in proc:', lastChar(inVal))
+
+    if (special(lastChar(inVal))) { // if special, add Last to  Cache and Out
+
+        log('S')
+        setCache(cache.concat(lastChar(inVal)))
+
+        out += lastChar(inVal)  //(readyTXT === undefined|| readyTXT.length === 0)  ? lastChar(inVal) : 
+        log('readyTXT', readyTXT, 'caache', cache)
+       
+        // let outVal = readyTXT
+            return [inVal, out]
+    }
+    else if 
+      
+        (special(prevChar(inVal)) === false) { // if common, and previous is common -> out /// special(lastChar(inVal)) === false && 
+            log('0-0')
+           
+        out += lastChar(inVal) //(readyTXT === undefined|| readyTXT.length === 0)  ? lastChar(inVal) : 
+        
+        
+        
+            
+            return  [inVal, out]
+        }
+        else { // (special(prevChar(inVal)) === true)
+            log('1-0')
+           
+                let tagLen = cache.length;
+                let outVal = '';
+                let beforeTag = out.slice(out.length-(tagLen+3),-(tagLen+2))
+                let joinedCache = Object.values(cache).map((m) => m).join('')
+                
+                let part0 = out.slice(0,-(tagLen))
+                let convertedTag = convert( joinedCache, beforeTag) //call Convert
+                
+                let partLast = lastChar(inVal)
+
+               out = part0.concat(convertedTag).concat(partLast) // invalNoTag + Tag + lastChar
+               
+            setCache('')
+
+            
+            return [inVal, out]
+        }
+    
+    }
+
+const processorTXT0 = (inVal, out) => { /// sequentually change proc0 to proc foo and fix the error
+        if (special(lastChar(inVal))) { //if (lastChar(inVal) === '#') {
+
+        // out += lastChar(inVal)
+        // log('s')
+        // log('out', out)
+        
+        log('S')
+        setCache(cache.concat(lastChar(inVal)))
+
+        out += lastChar(inVal) //(readyTXT === undefined|| readyTXT.length === 0)  ? lastChar(inVal) : 
+        log('readyTXT', readyTXT, 'caache', cache)
+       
+        // let outVal = readyTXT
+            return [inVal, out]
+    }
+
+    
+    else if (prevChar(inVal) !== '#' ) {
+
+        out += lastChar(inVal)
+    log('0-0')
+    log('out', out)
+    return [inVal,out]
+    }
+    else {
+
+        //out += lastChar(inVal)
+       log('1-0')
+        log('out', out)
+        
+        const changedOut = out.length ?  out.slice(0,-1).concat('<#>').concat(lastChar(inVal)) : lastChar(inVal)
+        //log('lastChar', out.slice(-2,-1) )
+        return [inVal,changedOut] //
+    }
+} 
+
+const handleIn = (e) => {
+
+    const inputString = e.target.value
+
+    setPreIn(inputString)
+
+    return 
+  
+}
+
+useLayoutEffect(()=>{                 //parser HTML, uses 'output' id in <div> at Display /// I use it to sync editor and ReadyTXT
+     log('preIn in Effect',preIn)
+    const processedTXT = processorTXT(preIn, readyTXT) //readyTXT.length > 0 ? processorTXT0(preIn, readyTXT) : processorTXT0(preIn,preIn) 
+    //if readyTXT not filled yet then use preIn to process
+
+
+ 
+   if (preIn.length >0) {  // to not send empties in state
+    
+    log('setEdit',processedTXT[0])
+    setEdit(preIn)
+
+    log('setReady',processedTXT[1])
+    setReadyTXT(processedTXT[1])
+   }
+   else {log('empties')}
+},[preIn])
+
 useLayoutEffect(()=>{                 //parser HTML, uses 'output' id in <div> at Display
    
     const $outP = $('#output') 
@@ -38,144 +211,15 @@ useLayoutEffect(()=>{                 //parser HTML, uses 'output' id in <div> a
 },[readyTXT])
 
 
-const proc = (inVal) => {
- 
-    let tmpStr = 'aaa<h1>'
-
-    let outVal = inVal;
-    let charLen = 1; let tagLen = 4; let codeTag = 6;
-    let cacheStr = '';
-
-    const lastChar = (inString) => inString.substring(inString.length-1)
-    const cutEdit = edit.substring(edit.length-charLen)
-    const cutEditTag = readyTXT.substring(readyTXT.length-tagLen)
-    const currentChar = lastChar(inVal); 
-    //log('currentChar',currentChar)
-   // const lastTag_inVal = inVal.substring(inVal.length-tagLen)
-
-//    log('cutEdit', cutEdit) ; log('inVal', currentChar)
-//    log('state::',  '\n', 'edit', edit, '\n', 'readyTXT', readyTXT)
-
-    const outVal_Foo = (tag, aCondition) => { //tag, adding condition - depends how many chars have to slice from end of inVal string
-        
-        // log('cutEditTag_outVal' ,cutEditTag, '-e', edit )
-        // log('aCondition', aCondition)
-        if (tag ==='code3') // CHECK LATER -- this exception may cause <code><code> issue
-        {   log('find CODE3')
-            return  !flags.code3 
-            ? readyTXT.slice(0, readyTXT.length - aCondition).concat(`<code>`) 
-                : readyTXT.slice(0, readyTXT.length - aCondition).concat(`</code>`) 
-        }
-        else 
-        return  !flags[`${tag}`] 
-            ? readyTXT.slice(0, readyTXT.length - aCondition).concat(`<${tag}>`) 
-                : readyTXT.slice(0, readyTXT.length - aCondition).concat(`</${tag}>`)   
-    }
-
-    if (currentChar === '*' && cutEdit === '*')      // <b> tag
-        {        
-        setFlags({...flags, b:!(flags.b)});    
-        return outVal_Foo('b', charLen)
-        }
-    else if (currentChar === 'i' && cutEdit === '\\')      // <b> tag
-        {        
-        setFlags({...flags, i:!(flags.i)});    
-        return outVal_Foo('i', charLen)
-        }
-    else if (currentChar === '\n' )      // <b> tag
-        {        
-        //setFlags({...flags, i:!(flags.i)}); 
-        //log('NEW LINE')   
-        return outVal_Foo('br/', 0)
-        }
-    
-        //FIX --  2 backtiks don't be parsed as tags
-
-    else if (currentChar === '`' && edit.length === 2)      // TRIPLE ``` at start 
-        
-        {   //log('Check 1st chunk of line', edit.slice(-3))
-            log('1` at start FIRED')
-            setFlags({...flags, code3:!(flags.code3)});  //  
-            return outVal_Foo('code3', 2)
-        }
-    else if (currentChar === '`' && readyTXT.slice(-7)==='<br/>``')      // TRIPLE ```
-        if (flags.code === true) 
-        {  //log('Check 1st chunk of line', edit.slice(-3))
-            log('THREE bactick FIRED for closing')
-            setFlags({...flags, code3:!(flags.code3)});  //  
-            return outVal_Foo('code3', 2)
-        }
-        else { log('THREE bactick FIRED')
-             setFlags({...flags, code3:!(flags.code3)});  //  
-            return outVal_Foo('code3', 2)
-        }
-
-    else if (currentChar === '`')      // <code> tag && flags.code = true
-       if(edit.length === 0) 
-        { log('first char at string is BACTICK')      
-        setFlags({...flags, code:!(flags.code)});    
-        return outVal_Foo('code',0)
-        }
-      else if(currentChar === '`' && edit.slice(-1) === '`') 
-        { log('return to plain text after 2 BT', edit)      
-        setFlags({...flags, code:!(flags.code)});    
-        return setEdit(edit.slice(0,charLen),'`' )
-        }
-        // else {
-        //     setFlags({...flags, code:!(flags.code)}); 
-        //     log('last ELSE FIRED')   
-        //     return outVal_Foo('code', codeTag+2)
-        // }
-
-    else if (currentChar === '#'  && cutEditTag === '<h2>')  // <h3> tag  
-            {
-            setFlags({...flags, h3:!(flags.h3), h2:false, h1:false})
-            return outVal_Foo('h3', tagLen);
-            }
-
-    else if (currentChar === '#'  && (cutEditTag === '<h1>' || cutEditTag === '</h1>'))  // <h2> tag
-            
-            {  let tempH2flag = flags.h2
-            setFlags({...flags, h2:!(flags.h2), h1:false}) // have to reset previous header flags to be able add closing current tag, as </h2>
-            return  outVal_Foo('h2', tagLen); 
-            }
-
-     else if (currentChar === '#')                     // <h1> tag
-            {setFlags({...flags, h1:!(flags.h1)})
-            return outVal_Foo('h1',0);
-            }
-     
-    else outVal = readyTXT.concat(currentChar)  //default ELSE adding lastCHar
-        
-   // log('outVal::', outVal)
-
-    return outVal
-} 
-const handleIn = (e) => {
-
-    const inputString = e.target.value
-
-    setEdit(inputString)
-
-    const proceedTxt = proc(inputString)
-        setReadyTXT(proceedTxt)
-        return 
-}
-
 const handleTest = () => {
-
-    const aTest = '0123456789\n``'
-
-    return log('edit Length:',aTest.length, ' ', aTest.slice(-3))//log('Substring',aTest.substring(0, aTest.length-6))
+    let cca = '789'
+    //const aTest = {0:'#',1:'#',2:'*', 3:'##'}
+    const aTest = '0123456789'
+   
+log('test:',aTest.slice(-2,-1))
+   
+    return 
 }
-
-// BACKSPACE handle
-
-//INSERT INSIDE TEXT - find place in EDIT and insert
-// if edit changed more than 1 char use Insert Foo -> find new text
-//, process char by char through Processor: part before insertion, and part after to USE PROPER FLAGS
-
-//for INSERT FOO - use varaible to cut the end of Edit after cursor position
 
 const HtmlView = (props) => {  //const { eDisp }  = props
   
@@ -192,8 +236,10 @@ const Preview = (props) => {  //const { eDisp }  = props
     </>
 } 
 
-//BIG RETURN
+//BIG RETURN before OUTPUT ---------------------------------
     return <> 
+
+
 
     <div>Markdown (under construct.)</div>
         <div id='editor'>editor
@@ -224,73 +270,20 @@ const Preview = (props) => {  //const { eDisp }  = props
 export default Markdown;
 
 
-// const checkLastChars = (inString, markChar) => {
-            
-//     const checkLast = (stringForCheck) => {
-//         let counter = 0;
-//         if (stringForCheck.length > 0 && lastChar(stringForCheck) === markChar) {
-//             counter = counter++
-//         return checkLast(stringForCheck.slice(-1)) 
-//         }
-//         else return counter;
-//     }
-//     return checkLast(inString)
-// }
+// else {
+//    log('1-0')
+//     log('out', out)
+//     const changedOut = out.slice(-2,-1).concat('<#>').lastChar(inVal)
+//     log('lastChar', out.slice(-2,-1) )
 
-//  const handleTest = () => {
-
-//     const aTest = 'sdsdf** *sd*sfa * sdf ** sdfsdf** sdfsdf sdfsgf#'
-
-//     return log('pre0',)
-// }
+//return [inVal,changedOut]
+    //log('procTest:', [inVal,changedTXT])
 
 
- // ---------- working part for entire entered txt
+// BACKSPACE handle
 
- // const tagsYN =(tag)=> {
+//INSERT INSIDE TEXT - find place in EDIT and insert
+// if edit changed more than 1 char use Insert Foo -> find new text
+//, process char by char through Processor: part before insertion, and part after to USE PROPER FLAGS
 
-//     const  flagSet = () => {
-//             log('h1:', flagsObj.h1)
-    
-//         let newFH1 = !flagsObj.h1
-//         flagsObj.h1 = newFH1
-//             log('h1:', flagsObj.h1)
-//         return setFlags({...flags, flagsObj});
-//     }
-// flagSet();
-
-//     return flagsObj.h1 === true ? `<${tag}>` : `</${tag}>`
-// }
-// const tagsYM =(tag)=> {
-
-//     const  flagSet = () => {
-//             log('b:', flagsObj.b)
-    
-//         let newFB = !flagsObj.b
-//         flagsObj.b = newFB
-//             log('b:', flagsObj.b)
-//         return setFlags({...flags, flagsObj});
-//     }
-// flagSet();
-
-//     return flagsObj.b === true ? `<${tag}>` : `</${tag}>`
-// }
-
-
-// const redur = () =>edit.split('').reduce((acc,elem,n) => {
-  
-//     switch(elem) {
-        
-//         case '#': return acc+ tagsYN('h1');
-//         case '*': return acc + tagsYM('b')
-//         default: return acc+elem}}
-        
-//         ,[]) 
-
-// useLayoutEffect((el)=> {
-//     const redurRes = redur()
-//     log('FX!', redurRes)
-//     setReadyTXT(redurRes)
-// }
-
-// , [edit])  
+//for INSERT FOO - use varaible to cut the end of Edit after cursor position
